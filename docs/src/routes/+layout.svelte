95ddf5d61dd17f18e36733d3e6f88c5f3c02ddcb
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2022-10-30 01:02:42 +0530

    init

---
+++ docs/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<main class="app">
+    <header>HEADER/NAV</header>
+
+    <div>
+        <slot />
+    </div>
+
+    <footer>
+        <p>
+            visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit
+        </p>
+    </footer>
+</main>
